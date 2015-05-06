2cf96fb5ac4fd2029e4b70b239d3c7ff03ac77c5
maximolopezb <15mlopez@iaa.edu.ec>
2015-05-06 11:40:46 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+print "Hello World" 
+print "My name is Maximo" 
+print "I am 17 years old \n This is Python"
+
+print 2015 #printing an integer
+print "2015"
+
+print 2015+4 #adding an integer
+print 34-5 #substracting  an integer
+print 34*8 # dmultiplying an integer
+
+#modulus
+print 10%34
+
+print "Odd or even numbers"
+print 17%3
+print 16%2 
+

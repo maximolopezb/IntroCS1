2c8fba8a752fdf7094c75d4cfaca4c04fb4eff4a
maximolopezb <15mlopez@iaa.edu.ec>
2015-05-06 11:40:07 -0500

    Create Homework1.py

---
+++ Homework1.py
@@ -0,0 +1,15 @@
+name = "Maximo"
+lastname = "Lopez"
+address = "Cdl. Laguna Dorada, Samborondon"
+email = "15mlopez@iaa.edu.ec"
+phonenumber = "593 4283 7371"
+
+print name
+print lastname
+print address
+print email
+print phonenumber
+
+print 'My name is', name, lastname
+print 'I live in:', address
+print 'You can contact me at my phone number:', phonenumber, 'or email me at:', email

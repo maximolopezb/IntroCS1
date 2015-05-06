aaec33aaeb4f2dcbb64caa9f1ac88a474222c080
maximolopezb <15mlopez@iaa.edu.ec>
2015-05-06 11:38:12 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,24 @@
+name= "Maximo Lopez"
+age= 17
+gpa=3.2
+
+print name
+print age
+print gpa
+
+print 'My name is', name, '.I am', age, 'Years Old. My gpa is', gpa
+print 'In ten years, I will be', age + 10
+
+ageinhumanyears=3
+ageindogyears= ageinhumanyears*7
+
+print 'My dog is', ageindogyears, 'Years old in dog years.'
+print 'But my dog is', ageinhumanyears, 'In human years.'
+
+mynumber = 5+4*5
+print mynumber
+
+print "Hello",2
+import math
+root= math.sqrt(81)
+print root

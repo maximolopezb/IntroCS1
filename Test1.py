48d3ac9311ab0a21afe0a7d4885b5b65e7066215
maximolopezb <15mlopez@iaa.edu.ec>
2015-05-06 11:37:24 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,9 @@
+operand1 = 95
+operand2=64.9
+
+print operand2 + operand1
+print operand1 - operand2
+print operand2 * operand1
+print operand1 / operand2
+print operand1 % operand2
+#I created the variables, gave them a value, amd then did some operations. 
